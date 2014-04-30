83aeb7ee158c264fbcad2470278d5733fa8d3e11
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-04-30 15:19:39 -0300

    Update remote repository at buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -291,7 +291,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: 'git@github.com:fmarquesfilho/coopera-site.git',
+          remote: 'git@github.com:coopera/coopera.github.io.git',
           branch: 'gh-pages',
           commit: true,
           push: true
